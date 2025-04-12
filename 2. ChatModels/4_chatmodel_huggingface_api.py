862bc8ad1b53cb4e54e944d37ad13f5d7659874a
Shubham8831 <shubuamsingh2002@gmail.com>
2025-04-13 05:15:50 +0530

    prompt template

--- 2. ChatModels/4_chatmodel_huggingface_api.py
+++ 2. ChatModels/4_chatmodel_huggingface_api.py
@@ -7,14 +7,18 @@ load_dotenv()  #HUGGINGFACEHUB_API_TOKEN become available in your Python script.
 
 llm = HuggingFaceEndpoint(
     repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0", #The model's path on Hugging Face
-    task="text-generation" # Type of task the model performs
+    task="text-generation", # Type of task the model performs
+    max_new_tokens=10
 )
 
 model = ChatHuggingFace(llm=llm) #This wraps the llm (TinyLlama model) into a chat interface so you can interact with it using prompts like a chatbot.
 
-result = model.invoke("what is the capital of India?")  #Used to call the model with a prompt.
+#result = model.invoke("what is the capital of India?")  #Used to call the model with a prompt.
 
 # print(result.content)
 
-result1 = model.invoke("What is cybersecurity?")
+# result1 = model.invoke("What is cybersecurity?")
+# print(result1.content)
+
+result1 = model.invoke([{"role": "user", "content": "What is cybersecurity?"}])
 print(result1.content)
